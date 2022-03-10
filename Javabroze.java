ab7442dfa05143cf440a44338d4a18c68c82d00c
kanomame23 <yk.kanonhamada@gmail.com>
2022-03-10 14:09:20 +0900

    コミット2回目

--- Javabroze.java
+++ Javabroze.java
@@ -1,3 +1,4 @@
+package sample;
 public class Javabronze {
     public static void main(String[] args) {
         int i = 0;
@@ -7,3 +8,37 @@ public class Javabronze {
         System.out.println(i);
     }
 }
+
+//結果は5になる
+
+public static void main(String[] args) {
+			
+    int i = 1;
+    int j = 2;
+    while (i < 10) {
+        i = j * i;
+    }
+    System.out.println("i=" + i + ", j=" + j);
+}
+
+//i=16, j=2
+
+package sample;
+
+public class JavaSampleClass {
+
+		public static void main(String[] args) {
+			
+			int i = 1;
+			int j = 10;
+			while (i < j) {
+				System.out.println("LOOP");
+				i *= 2;
+				j /= 2;
+			}
+		}
+
+}
+
+//結果はLOOPが2回表示される
+
