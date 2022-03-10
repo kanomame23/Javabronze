83842251e80fd62a9646e6fc97697f7b1efb0c57
kanomame23 <yk.kanonhamada@gmail.com>
2022-03-10 11:49:44 +0900

    Javaファイル作成

---
+++ Javabroze.java
@@ -0,0 +1,9 @@
+public class Javabronze {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            i++;
+        }
+        System.out.println(i);
+    }
+}
