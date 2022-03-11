015ae26bbcb0ac49c929374b1cf0dae4afb9439e
kanomame23 <yk.kanonhamada@gmail.com>
2022-03-11 17:46:08 +0900

    条件分岐の練習if文

--- Javabroze.java
+++ Javabroze.java
@@ -42,3 +42,30 @@ public class JavaSampleClass {
 
 //結果はLOOPが2回表示される
 
+class Main {
+    public static void main(String[] args) {
+      // 条件式に直接trueをいれてください
+      if (true) {
+        System.out.println("条件式がtrueのため、出力されます");
+      }
+      
+      // 条件式に直接falseをいれてください
+      if (false) {
+        System.out.println("条件式がfalseのため、出力されません");
+      }
+      
+      int x = 5;
+      // if文を用いて、変数xが2より大きいとき「xは2より大きい」と出力してください
+      if (x > 2) {
+        System.out.println("xは2より大きい");
+      }
+      
+      // if文を用いて、変数xが8より大きいとき「xは8より大きい」と出力してください
+      if (x > 8) {
+        System.out..println("xは8より大きい");
+      }
+      
+    }
+  }
+
+  //条件分岐の練習
