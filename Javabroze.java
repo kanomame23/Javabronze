b6ea4be73e8d0a5045f09dcba314dde26cc19dc3
kanomame23 <yk.kanonhamada@gmail.com>
2022-03-15 12:44:27 +0900

    条件式の勉強

--- Javabroze.java
+++ Javabroze.java
@@ -69,3 +69,25 @@ class Main {
   }
 
   //条件分岐の練習
+
+  class Main {
+    public static void main(String[] args) {
+      int n = 0;
+      
+      switch (n) {
+        case 1:
+          System.out.println("大吉です");
+          break;
+        case 2:
+          System.out.println("中吉です");
+          break;
+        // defaultを用いて、変数nの値がどのcaseにも合致しない場合の処理を記述してください
+        default:
+          System.out.println("凶です");
+        
+        
+      }
+    }
+  }
+
+    //switch文
