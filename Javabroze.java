6d4da548c3a7fc403e5bddb936a6c040ca2e64f8
kanomame23 <yk.kanonhamada@gmail.com>
2022-03-16 11:44:03 +0900

    メソッドの戻り値まで理解

--- Javabroze.java
+++ Javabroze.java
@@ -91,3 +91,28 @@ class Main {
   }
 
     //switch文
+
+    class Main {
+      public static void main(String[] args) {
+        // 変数numbersに、与えられた数字の配列を代入してください
+        int[] numbers = {1, 4, 6, 9, 13, 16};
+        
+        int oddSum = 0;
+        int evenSum = 0;
+        
+        // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
+        for (int number : numbers) {
+          if (number % 2 == 0) {
+            evenSum += number;
+          } else {
+            oddSum += number;
+          }
+        }
+    
+        System.out.println("奇数の和は" + oddSum + "です");
+        System.out.println("偶数の和は" + evenSum + "です");
+      }
+    }
+
+    //拡張for文
+    
