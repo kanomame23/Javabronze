0c926e0217c66d62b42efe7d2e939c997d880cc0
kanomame23 <yk.kanonhamada@gmail.com>
2022-03-23 11:46:25 +0900

    scanner.next();について

--- Javabroze.java
+++ Javabroze.java
@@ -283,3 +283,39 @@ class Main {
         }
   
           //264行目の()の中に、"name"を入れることで、Santa Claraと表示されるようになる
+
+          import java.util.Scanner;
+
+          class Main {
+            public static void main(String[] args) {
+              Scanner scanner = new Scanner(System.in);
+              
+              System.out.print("名前：");
+              // 文字列の入力を受け取ってください
+              String firstName = scanner.next();
+              
+              System.out.print("名字：");
+              // 文字列の入力を受け取ってください
+              String lastName = scanner.next();
+              
+              System.out.print("年齢：");
+              // 整数の入力を受け取ってください
+              int age = scanner.nextInt();
+              
+              System.out.print("身長(m)：");
+              // 小数の入力を受け取ってください
+              double height = scanner.nextDouble();
+              
+              System.out.print("体重(kg)：");
+              // 小数の入力を受け取ってください
+              double weight = scanner.nextDouble();
+              
+              Person.printData(Person.fullName(firstName, lastName), age, height, weight);
+            }
+          }
+
+          //Scanner scanner = new Scannwe(System.in);でscannerデータを初期化
+          //String name = scanner.next();でコンソールに入力されてから次の処理に移る
+          
+          
+          
