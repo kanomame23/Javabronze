e7409f949ddeb95f47897449e6ad721a6cee4c2b
kanomame23 <yk.kanonhamada@gmail.com>
2022-03-30 12:16:16 +0900

    sampleファイルの作成

---
+++ sample.java
@@ -0,0 +1,10 @@
+public class sample {
+    public static void main(String[] args) {
+        int num01 = 10;
+        int num02 = 20;
+        int num03 = 30;
+    }
+    System.out.println(num01);
+    System.out.println(num02);
+    System.out.println(num03);
+}
